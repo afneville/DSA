60509e502ff8597d92c28ea83a75f1d92bccdce6
Alexander Neville <git@afneville.com>
2024-01-31 20:08:22 +0000

    update linked list tests

--- tests/linked_list_test.cpp
+++ tests/linked_list_test.cpp
@@ -3,10 +3,22 @@
 #include <iostream>
 #include <stdexcept>
 
-TEST(LinkedListTest, DefaultIntialisation) {}
-TEST(LinkedListTest, ListIntialisation) {}
-TEST(LinkedListTest, SubscriptOperator) {}
-TEST(LinkedListTest, AppendMethod) {}
-TEST(LinkedListTest, PrependMethod) {}
-TEST(LinkedListTest, InsertMethod) {}
-TEST(LinkedListTest, DropMethod) {}
+TEST(LinkedListTest, DefaultIntialisation) {
+    LinkedList<int> l {};
+    ASSERT_EQ(lengthOf(l), 0);
+}
+TEST(LinkedListTest, ListIntialisation) {
+    LinkedList<int> l {3,4,5};
+    ASSERT_EQ(lengthOf(l), 3);
+}
+TEST(LinkedListTest, SubscriptOperator) {
+    LinkedList<int> l {3,4,5};
+    ASSERT_EQ(l[0], 3);
+    ASSERT_EQ(l[1], 4);
+    ASSERT_EQ(l[2], 5);
+    ASSERT_THROW(l[3], std::out_of_range);
+}
+// TEST(LinkedListTest, AppendMethod) {}
+// TEST(LinkedListTest, PrependMethod) {}
+// TEST(LinkedListTest, InsertMethod) {}
+// TEST(LinkedListTest, DropMethod) {}
