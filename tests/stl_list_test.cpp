7aec4032615bcf3ce2ad749a0af1857a492806ed
Alexander Neville <git@afneville.com>
2024-02-10 11:42:20 +0000

    Update tests for STL containers

--- tests/stl_list_test.cpp
+++ tests/stl_list_test.cpp
@@ -1,5 +1,5 @@
+// #include "../templates/deque.hpp"
 #include "../templates/vector.hpp"
-#include "../templates/deque.hpp"
 #include <gtest/gtest.h>
 #include <iostream>
 #include <stdexcept>
@@ -7,10 +7,13 @@
 // Test Fixture
 template <typename T> class StandardTemplateList : public testing::Test {
 public:
+    T l1{1, 2, 3};
+    T l2{4, 5, 6, 7};
+    T l3{8, 9, 10, 11, 12};
 };
 
 // Type Paramaters
-using ListImplementations = ::testing::Types<Vector<int>, Deque<int>>;
+using ListImplementations = ::testing::Types<Vector<int>>;
 TYPED_TEST_SUITE(StandardTemplateList, ListImplementations);
 
 // Tests
@@ -25,190 +28,148 @@ TYPED_TEST(StandardTemplateList, ListIntialisation) {
 }
 
 TYPED_TEST(StandardTemplateList, SubscriptOperator) {
-    TypeParam l{3, 4, 5};
-    ASSERT_EQ(l[0], 3);
-    ASSERT_EQ(l[1], 4);
-    ASSERT_EQ(l[2], 5);
+    ASSERT_EQ(this->l1[0], 1);
+    ASSERT_EQ(this->l1[1], 2);
+    ASSERT_EQ(this->l1[2], 3);
 }
 
 TYPED_TEST(StandardTemplateList, SafeSubscriptOperator) {
-    TypeParam l{3, 4, 5};
-    ASSERT_EQ(l.at(0), 3);
-    ASSERT_EQ(l.at(1), 4);
-    ASSERT_EQ(l.at(2), 5);
-    ASSERT_THROW(l.at(3), std::out_of_range);
+    ASSERT_EQ(this->l1.at(0), 1);
+    ASSERT_EQ(this->l1.at(1), 2);
+    ASSERT_EQ(this->l1.at(2), 3);
+    ASSERT_THROW(this->l1.at(3), std::out_of_range);
 }
 
-
 TYPED_TEST(StandardTemplateList, CopyConstructor) {
-    TypeParam l1{3, 4, 5};
-    TypeParam l2(l1);
-    ASSERT_EQ(l1[0], l2[0]);
-    l1[0] = 6;
-    ASSERT_NE(l1[0], l2[0]);
+    TypeParam l4(this->l1);
+    ASSERT_EQ(l4[0], this->l1[0]);
+    this->l1[0] = 6;
+    ASSERT_NE(this->l1[0], l4[0]);
 }
 
 TYPED_TEST(StandardTemplateList, MoveConstructor) {
-    TypeParam l1{3, 4, 5};
-    TypeParam l2(std::move(l1));
-    ASSERT_EQ(l2.size(), 3);
-    ASSERT_EQ(l2[0], 3);
-    ASSERT_EQ(l1.size(), 0);
-    ASSERT_THROW(l1.at(0), std::out_of_range);
+    int expected_size = this->l1.size();
+    int expected_element = this->l1[0];
+    TypeParam l4(std::move(this->l1));
+    ASSERT_EQ(l4.size(), expected_size);
+    ASSERT_EQ(l4[0], expected_element);
+    ASSERT_EQ(this->l1.size(), 0);
+    ASSERT_THROW(this->l1.at(0), std::out_of_range);
 }
 
-TYPED_TEST(StandardTemplateList, CopySwapMoveAssignment) {
-    TypeParam l1{3, 4, 5};
-    TypeParam l2{6, 7, 8, 9};
-    TypeParam l3{10, 11, 12, 13, 14};
-
-    l1 = l2;
-    ASSERT_EQ(l1.size(), 4);
-    ASSERT_EQ(l1[0], l2[0]);
-    l1[0] = 7;
-    ASSERT_NE(l1[0], l2[0]);
-
-    l1 = std::move(l3);
-    ASSERT_EQ(l1.size(), 5);
-    ASSERT_EQ(l1[3], 13);
-    ASSERT_THROW(l3.at(0), std::out_of_range);
+TYPED_TEST(StandardTemplateList, CopyAssignment) {
+    this->l1 = this->l3;
+    ASSERT_EQ(this->l1.size(), this->l3.size());
+    ASSERT_EQ(this->l1[0], this->l3[0]);
+    this->l1[0] += 999; // change the value of the element
+    ASSERT_NE(this->l1[0], this->l3[0]);
 }
 
-TYPED_TEST(StandardTemplateList, TestFrontReference) {
-    TypeParam l1{3, 4, 5};
-    ASSERT_EQ(l1.front(), 3);
+TYPED_TEST(StandardTemplateList, MoveAssignment) {
+    int expected_size = this->l2.size();
+    int expected_element = this->l2[0];
+    this->l1 = std::move(this->l2);
+    ASSERT_EQ(this->l1.size(), expected_size);
+    ASSERT_EQ(this->l1[0], expected_element);
+    ASSERT_EQ(this->l2.size(), 0);
+    ASSERT_THROW(this->l2.at(0), std::out_of_range);
 }
 
-TYPED_TEST(StandardTemplateList, TestBackReference) {
-    TypeParam l1{3, 4, 5};
-    ASSERT_EQ(l1.back(), 5);
+TYPED_TEST(StandardTemplateList, FrontReference) {
+    ASSERT_EQ(this->l1.front(), this->l1[0]);
+    this->l1.front() = 100;
+    ASSERT_EQ(this->l1[0], 100);
 }
 
-TYPED_TEST(StandardTemplateList, TestPushBack) {
-    TypeParam l1{3, 4, 5};
-    l1.push_back(6);
-    ASSERT_EQ(l1.back(), 6);
-    ASSERT_EQ(l1.size(), 4);
+TYPED_TEST(StandardTemplateList, BackReference) {
+    ASSERT_EQ(this->l1.back(), this->l1[this->l1.size() - 1]);
+    this->l1.back() = 100;
+    ASSERT_EQ(this->l1[this->l1.size() - 1], 100);
 }
 
-TYPED_TEST(StandardTemplateList, TestPushFront) {
-    TypeParam l1{3, 4, 5};
-    l1.push_front(2);
-    ASSERT_EQ(l1.front(), 2);
-    ASSERT_EQ(l1.size(), 4);
+TYPED_TEST(StandardTemplateList, PushBack) {
+    int test_element = 4;
+    int expected_length = 4;
+    this->l1.push_back(4);
+    ASSERT_EQ(this->l1.back(), test_element);
+    ASSERT_EQ(this->l1.size(), expected_length);
 }
 
-TYPED_TEST(StandardTemplateList, TestPopBack) {
-    TypeParam l1{3, 4, 5};
-    l1.pop_back();
-    ASSERT_EQ(l1.back(), 4);
-    ASSERT_EQ(l1.size(), 2);
+TYPED_TEST(StandardTemplateList, PushFront) {
+    int test_element = 0;
+    int expected_length = 4;
+    this->l1.push_front(test_element);
+    ASSERT_EQ(this->l1.front(), test_element);
+    ASSERT_EQ(this->l1.size(), expected_length);
 }
 
-TYPED_TEST(StandardTemplateList, TestPopFront) {
-    TypeParam l1{3, 4, 5};
-    l1.pop_front();
-    ASSERT_EQ(l1.front(), 4);
-    ASSERT_EQ(l1.size(), 2);
-}
-// TYPED_TEST(StandardTemplateList, TestErase) {}
-TYPED_TEST(StandardTemplateList, TestClear) {
-    TypeParam l1{3, 4, 5};
-    l1.clear();
-    ASSERT_EQ(l1.size(), 0);
-    ASSERT_THROW(l1.at(0), std::out_of_range);
+TYPED_TEST(StandardTemplateList, PopBack) {
+    int expected_length = 2;
+    int expected_element = 2;
+    this->l1.pop_back();
+    ASSERT_EQ(this->l1.back(), 2);
+    ASSERT_EQ(this->l1.size(), 2);
 }
 
+TYPED_TEST(StandardTemplateList, PopFront) {
+    int expected_length = 2;
+    int expected_element = 2;
+    this->l1.pop_front();
+    ASSERT_EQ(this->l1.front(), 2);
+    ASSERT_EQ(this->l1.size(), 2);
+}
 
+TYPED_TEST(StandardTemplateList, Clear) {
+    this->l1.clear();
+    ASSERT_EQ(this->l1.size(), 0);
+    ASSERT_THROW(this->l1.at(0), std::out_of_range);
+}
 
+TYPED_TEST(StandardTemplateList, Erase) {
+    int expected_length = this->l3.size();
+    this->l3.erase(this->l3.size() - 1);
+    ASSERT_EQ(this->l3.size(), --expected_length);
+    this->l3.erase(0);
+    ASSERT_EQ(this->l3.size(), --expected_length);
+    this->l3.erase(1);
+    ASSERT_EQ(this->l3.size(), --expected_length);
+    ASSERT_EQ(this->l3.front(), 9);
+    ASSERT_EQ(this->l3.back(), 11);
+}
 
+TYPED_TEST(StandardTemplateList, BeginIterator) {
+    ASSERT_EQ(*(this->l3.begin()), this->l3.front());
+}
 
+TYPED_TEST(StandardTemplateList, EndIterator) {
+    ASSERT_EQ(*(--(this->l3.end())), this->l3.back());
+}
 
+TYPED_TEST(StandardTemplateList, ReverseBeginIterator) {
+    ASSERT_EQ(*(this->l3.rbegin()), this->l3.back());
+}
 
+TYPED_TEST(StandardTemplateList, ReverseEndIterator) {
+    ASSERT_EQ(*(--(this->l3.end())), this->l3.back());
+}
 
+TYPED_TEST(StandardTemplateList, ForwardIterator) {
+    int expected_element = this->l3.front();
+    for (auto element : this->l3) {
+        ASSERT_EQ(element, expected_element++);
+    }
+}
 
+TYPED_TEST(StandardTemplateList, ReverseIterator) {
+    int expected_element = this->l3.back();
+    for (auto i = this->l3.rbegin(); i != this->l3.rend(); ++i) {
+        ASSERT_EQ(*i, expected_element--);
+    }
+}
 
-
-
-
-
-//
-// TYPED_TEST(StandardTemplateList, AppendMethod) {
-//     TypeParam l1{};
-//     ASSERT_EQ(lengthOf(l1), 0);
-//     ASSERT_THROW(l1[0], std::out_of_range);
-//     for (auto i : {0, 1, 2, 3, 4}) {
-//         l1.append(i);
-//         ASSERT_NO_THROW(l1[i]);
-//         ASSERT_EQ(l1[i], i);
-//         ASSERT_EQ(l1[i], i);
-//         ASSERT_EQ(l1[i], i);
-//     }
-//     ASSERT_THROW(l1[5], std::out_of_range);
-// }
-//
-// TYPED_TEST(ListTest, PrependMethod) {
-//     TypeParam l1{3, 4, 5, 6, 7};
-//     l1.prepend(2);
-//     ASSERT_EQ(lengthOf(l1), 6);
-//     l1.prepend(1);
-//     ASSERT_EQ(lengthOf(l1), 7);
-//     ASSERT_EQ(l1[0], 1);
-//     ASSERT_EQ(l1[1], 2);
-// }
-//
-// TYPED_TEST(ListTest, InsertMiddle) {
-//     TypeParam l1{3, 4, 5, 6, 7};
-//     l1.insert(3, 10);
-//     ASSERT_EQ(lengthOf(l1), 6);
-//     ASSERT_EQ(l1[3], 10);
-// }
-//
-// TYPED_TEST(ListTest, InsertStart) {
-//     TypeParam l1{3, 4, 5, 6, 7};
-//     l1.insert(0, 2);
-//     ASSERT_EQ(lengthOf(l1), 6);
-//     l1.insert(0, 1);
-//     ASSERT_EQ(lengthOf(l1), 7);
-//     ASSERT_EQ(l1[0], 1);
-//     ASSERT_EQ(l1[1], 2);
-// }
-//
-// TYPED_TEST(ListTest, InsertEnd) {
-//     TypeParam l1{3, 4, 5, 6, 7};
-//     l1.insert(5, 8);
-//     ASSERT_EQ(lengthOf(l1), 6);
-//     ASSERT_EQ(l1[5], 8);
-//     ASSERT_THROW(l1.insert(7, 10), std::out_of_range);
-// }
-//
-// TYPED_TEST(ListTest, DropMiddle) {
-//     TypeParam l1{3, 4, 5, 6, 7};
-//     ASSERT_EQ(l1.drop(2), 5);
-//     ASSERT_EQ(lengthOf(l1), 4);
-//     ASSERT_EQ(l1[1], 4);
-//     ASSERT_EQ(l1[2], 6);
-// }
-//
-// TYPED_TEST(ListTest, DropStart) {
-//     TypeParam l1{3, 4, 5, 6, 7};
-//     ASSERT_EQ(l1.drop(0), 3);
-//     ASSERT_EQ(lengthOf(l1), 4);
-//     ASSERT_EQ(l1.drop(0), 4);
-//     ASSERT_EQ(lengthOf(l1), 3);
-//     ASSERT_EQ(l1[0], 5);
-//     ASSERT_EQ(l1[1], 6);
-// }
-//
-// TYPED_TEST(ListTest, DropEnd) {
-//     TypeParam l1{3, 4, 5, 6, 7};
-//     ASSERT_EQ(l1.drop(4), 7);
-//     ASSERT_THROW(l1.drop(4), std::out_of_range);
-//     ASSERT_EQ(lengthOf(l1), 4);
-// }
-//
-// TYPED_TEST(ListTest, DropStartEnd) {
-//     TypeParam l1{2};
-//     ASSERT_EQ(l1.drop(0), 2);
-//     ASSERT_EQ(lengthOf(l1), 0);
-// }
+TYPED_TEST(StandardTemplateList, RandomAccessIterator) {
+    auto begin = this->l3.begin();
+    auto end = this->l3.end();
+    ASSERT_EQ(*(begin + 2), this->l3[2]);
+    ASSERT_EQ(*(end - 2), this->l3[this->l3.size() - 2]);
+}
